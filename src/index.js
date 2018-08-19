1f208dc04bf6b7965fac8db21297fdc2992dd08e
Krystyna Szurmańska <kszurmanska@gmail.com>
2018-08-19 10:00:48 +0200

    Add routes and presentational components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './devtools/DevTools';
+//import Navigation from './presentational/navigation.component';
+import routes from './routes';
+//import { getCountries } from './actions/actions-countries';
 
 render(
     <Provider store={store}>
-       <div>
-        <h1>Inicjalizacja projektu</h1>
-        <DevTools />
-        </div>
-    </Provider>,
+<div>
+        <Router history={hashHistory} routes={routes} />
+<DevTools />
+  </div>     
+    </Provider >,
     document.getElementById('root')
 );
+
+//store.dispatch(getCountries)
