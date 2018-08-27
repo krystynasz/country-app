2f24a77c81f492acaac9d31b41d4fd6e094f5dec
Krystyna Szurmańska <kszurmanska@gmail.com>
2018-08-27 17:13:05 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './devtools/DevTools';
+import { GET_COUNTRIES } from './actions/actions-countries';
 
 render(
     <Provider store={store}>
        <div>
         <h1>Inicjalizacja projektu</h1>
+        <button onClick={() => store.dispatch({type:GET_COUNTRIES})}>Kliknij tutaj i załaduj listę krajów.</button>
         <DevTools />
         </div>
     </Provider>,
